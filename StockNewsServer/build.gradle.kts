14e9ee1bc0a85de83813f5d340a9fd4fe9d464af
CASL0 <gin.0046.822@gmail.com>
2023-11-22 00:37:36 +0900

    build: spring-webとfcm-adminのdep追加

--- StockNewsServer/build.gradle.kts
+++ StockNewsServer/build.gradle.kts
@@ -19,8 +19,9 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.google.firebase:firebase-admin:9.2.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
